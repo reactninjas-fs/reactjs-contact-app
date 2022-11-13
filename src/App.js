95b228295b5edba4b7c30b80064c90945b1a6a50
bzceval <busraaceval@gmail.com>
2022-11-13 11:38:15 +0300

    add main responsive setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App"> 
-    
+    <div className="App">
+    <div className="container">
+      <div className="row align-items-md-stretch">
+
+        <div className="col-md-6 mb-2">
+          <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Add Contact</h2>
+            <p>
+              
+            </p>
+            <button className="btn btn-outline-light" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+
+        <div className="col-md-6 mb-2">
+          <div className="h-100 p-5 bg-light border rounded-3">
+            <h2>Contact List</h2>
+            <p>
+              
+            </p>
+            <button className="btn btn-outline-secondary" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
     </div>
   );
 }
