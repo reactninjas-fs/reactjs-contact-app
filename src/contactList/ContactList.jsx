e8575670678c99541b6b82a68af333e34f044615
bzceval <busraaceval@gmail.com>
2022-11-13 15:37:43 +0300

    done data read

--- src/contactList/ContactList.jsx
+++ src/contactList/ContactList.jsx
@@ -2,23 +2,31 @@ import React from 'react'
 import user from '../assets/user.png'
 import { AiFillDelete } from 'react-icons/ai';
 import { FiEdit } from 'react-icons/fi';
+import { useFetch } from '../firebase/firebasefunction';
 
 const ContactList = () => {
+  const {contactList, loading}=useFetch()
   return (
-    <div className="list-group w-auto">
+    <div> 
+      {
+        loading ? "loding" : contactList?.length===0 ? "no data" : contactList?.map((item)=>
+        <div className="list-group w-auto">
         <div className="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
           <img src={user} alt="twbs" width={40} height={40} className="rounded-circle flex-shrink-0" />
           <div className="d-flex gap-2 w-100 justify-content-between">
             <div>
-              <h6 className="mb-0">Username: </h6>
-              <p className="mb-0 opacity-75">Phone Number: </p>
-              <p className="mb-0 opacity-75">Gender: </p>
+              <h6 className="mb-0">Username: {item.username}</h6>
+              <p className="mb-0 opacity-75">Phone Number: {item.phoneNumber} </p>
+              <p className="mb-0 opacity-75">Gender: {item.gender}</p>
             </div>
             <small className="opacity-50 text-nowrap"><AiFillDelete className='mx-1'/><FiEdit className='mx-1'/></small>
           </div>
         </div>
       </div>
+      )
+      }
+    </div> 
   )
 }
 
-export default ContactList
+export default ContactList 
--- src/firebase/firebasefunction.js
+++ src/firebase/firebasefunction.js
@@ -1,7 +1,8 @@
-import { getDatabase, ref, push, set } from "firebase/database";
+import { getDatabase, ref, push, set, onValue} from "firebase/database";
 import firebase from "./config";
+import {useState, useEffect} from 'react'
 
-// Create a new post reference with an auto-generated id
+// data oluşturma
 export const AddUser=(info)=>{
     const db=getDatabase(firebase);
     const userRef=ref(db, 'users/');
@@ -11,4 +12,24 @@ export const AddUser=(info)=>{
         phoneNumber: info.phoneNumber,
         gender: info.gender
     });
+}
+
+// data okuma
+export const useFetch=()=>{
+    const [contactList, setContactList] = useState();
+    const [loading, setLoading] = useState()
+    useEffect(() => { 
+      const db=getDatabase(firebase);
+      const userRef=ref(db, 'users/');
+      onValue(userRef,(snapshot)=>{
+          const data=snapshot.val();
+          const userArray=[]
+          for(let id in data){
+              userArray.push({id, ...data[id]})
+          }
+          setContactList(userArray)
+          setLoading(false)
+      })
+    }, [])
+    return {contactList, loading}
 }
